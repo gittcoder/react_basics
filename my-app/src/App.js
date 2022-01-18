5b1e5883e70510cd3d0999b6186ae9df57989e95
gittcoder <adityaavinash.n2020@vitstudent.ac.in>
2022-01-18 09:10:54 +0530

    c-2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,25 @@
+import Hello from './Hello'
+import React, { Component} from 'react';
 
-import './App.css';
-
-function App() {
+class App extends Component
+{
+    state={
+      hello : [
+        {name:'adi', age:19 , about:'Nothing',id:1},
+        {name:'axi', age:29 , about:'something',id:2},
+        {name:'azi', age:39 , about:'Everything',id:3}
+      ]
+    }
+    render()
+    {
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* <Hello name ="adi" age="19" about="nothing" /> */}
+        <Hello hello={this.state.hello}/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,9 +28,11 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
+}
 }
 
 export default App;
